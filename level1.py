21acdfd59523ec7d647313574156b2684e77b7dc
kavitha <kavinu2010@gmail.com>
2025-02-01 17:05:26 +0100

    function with paramter

--- level1.py
+++ level1.py
@@ -1,4 +1,10 @@
-'''A simple function with o arguments and no return value'''
+# A simple function with o arguments and no return value 
 def greet():
   print('Hello, world')
 greet()
+
+# A function that takes paramters
+
+def greet1(name):
+  print(f'Hello, {name}!')
+greet1('Akash')
